5a147cab497090d8dd68a1551873ec94bc3eb3c0
Ahmet Cengizhan Dirican <acdirican@gmail.com>
2022-07-22 16:13:19 +0300

    database export file updated

--- inventorymaster_db.sql
+++ inventorymaster_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 21 Tem 2022, 01:07:39
+-- Üretim Zamanı: 22 Tem 2022, 15:11:14
 -- Sunucu sürümü: 10.4.24-MariaDB
 -- PHP Sürümü: 7.4.28
 
@@ -38,37 +38,61 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Tablo için tablo yapısı `products`
+-- Tablo için tablo yapısı `product`
 --
 
-CREATE TABLE `products` (
+CREATE TABLE `product` (
   `ID` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
-  `quantity` float NOT NULL
+  `quantity` float NOT NULL,
+  `SupplierID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Tablo döküm verisi `products`
+-- Tablo döküm verisi `product`
 --
 
-INSERT INTO `products` (`ID`, `name`, `quantity`) VALUES
-(1, 'Sony SX120', 50),
-(2, 'Aiwa AA345', 0),
-(4, 'Sony A785', 607),
-(5, 'Amonia S652', 456),
-(6, 'Gamzi R56', 0),
-(7, 'Lanza ER 456', 41),
-(8, 'Kania Tami 34', 55),
-(9, 'Hanio ER T56', 3);
+INSERT INTO `product` (`ID`, `name`, `quantity`, `SupplierID`) VALUES
+(15, 'Beko T145', 56, 1),
+(16, 'Arçelik AK567 A+', 77, 1),
+(20, 'Yellow 56 T 32', 4, 1),
+(22, 'Karma Z45', 0, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Tablo döküm verisi `supplier`
+--
+
+INSERT INTO `supplier` (`ID`, `Name`) VALUES
+(1, 'Farma'),
+(2, 'Gama'),
+(3, 'Zama Co.');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
 --
 
 --
--- Tablo için indeksler `products`
+-- Tablo için indeksler `product`
 --
-ALTER TABLE `products`
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_Supplier` (`SupplierID`);
+
+--
+-- Tablo için indeksler `supplier`
+--
+ALTER TABLE `supplier`
   ADD PRIMARY KEY (`ID`);
 
 --
@@ -76,10 +100,26 @@ ALTER TABLE `products`
 --
 
 --
--- Tablo için AUTO_INCREMENT değeri `products`
+-- Tablo için AUTO_INCREMENT değeri `product`
 --
-ALTER TABLE `products`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `product`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Dökümü yapılmış tablolar için kısıtlamalar
+--
+
+--
+-- Tablo kısıtlamaları `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `FK_Supplier` FOREIGN KEY (`SupplierID`) REFERENCES `supplier` (`ID`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
